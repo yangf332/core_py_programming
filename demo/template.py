e0642778a9abab2d628424f1873c1d8fff7432ee
yangf332 <yangf332@gmail.com>
2013-03-02 21:48:06 +0800

    add index

---
+++ demo/template.py
@@ -0,0 +1,19 @@
+#/usr/bin/python
+#template.py
+
+'''document'''
+
+# 定义全局变量
+debug = True
+
+# 定义类
+class FooClass:
+	'''FooClass'''
+	pass
+
+def test():
+	'''test function'''
+
+
+if __name__ == '__main__':
+	test()
