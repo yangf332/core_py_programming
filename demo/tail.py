2afa03941642a85b72103331ee90c2ef3cab3640
yangf332 <yangf332@gmail.com>
2013-05-29 16:30:57 +0800

    convert space

--- demo/tail.py
+++ demo/tail.py
@@ -5,22 +5,22 @@
 import time
 
 def tail(f):
-	f.seek(0, 2)  # EOF
-	while True:
-		line = f.readline()
-		if not line:
-			time.sleep(0.1)
-			continue
-		yield line
+    f.seek(0, 2)  # EOF
+    while True:
+        line = f.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
 
 def grep(lines, searchtext):
-	for line in lines:
-		if searchtext in line: yield line
+    for line in lines:
+        if searchtext in line: yield line
 
 
 if __name__ == '__main__':
-	w = tail(open(u'/var/www/html/test.txt'))
-	pylines = grep(w, 'python')
-	for line in pylines:
-		print(line)
+    w = tail(open(u'/var/www/html/test.txt'))
+    pylines = grep(w, 'python')
+    for line in pylines:
+        print(line)
 
